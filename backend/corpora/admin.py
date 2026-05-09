f325093c6f06a888c18fbac6594071ab60f47467
Mark <mark.yarullin@gmail.com>
2026-05-09 21:45:11 +0400

    added models and migration

--- backend/corpora/admin.py
+++ backend/corpora/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Volume, Work, Token
 
-# Register your models here.
+
+@admin.register(Volume)
+class VolumeAdmin(admin.ModelAdmin):
+    list_display = ("id", "number", "title", "author", "uploaded_at")
+    search_fields = ("title", "author")
+
+
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "genre", "author", "volume")
+    list_filter = ("volume", "genre", "language")
+    search_fields = ("title", "plain_text")
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ("id", "work", "text_position", "text", "lemma", "pos")
+    search_fields = ("text", "lemma")
--- backend/corpora/models.py
+++ backend/corpora/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class Volume(models.Model):
+    source_id = models.CharField(max_length=20, blank=True)
+
+    number = models.IntegerField(null=True, blank=True)
+
+    author = models.CharField(max_length=50)
+    title_short = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+
+    xml_file = models.FileField(upload_to="tei_volumes/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Work(models.Model):
+    volume = models.ForeignKey(Volume, on_delete=models.CASCADE, related_name="works")
+
+    source_id = models.CharField(max_length=20)
+    note = models.CharField(max_length=50, blank=True)
+    page_number = models.IntegerField(null=True, blank=True)
+
+    date_from = models.CharField(max_length=20, blank=True)
+    date_to = models.CharField(max_length=20, blank=True)
+    place = models.CharField(max_length=50, blank=True)
+
+    author = models.CharField(max_length=50)
+    language = models.CharField(max_length=20, blank=True)
+    title_desc = models.CharField(max_length=200, blank=True)
+    title_short = models.CharField(max_length=50, blank=True)
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=20)
+
+    plain_text = models.TextField()
+    raw_xml = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Token(models.Model):
+    work = models.ForeignKey(Work, on_delete=models.CASCADE, related_name="tokens")
+
+    text_position = models.IntegerField(db_index=True)
+    text = models.CharField(max_length=20)
+    lemma = models.CharField(max_length=20, blank=True, db_index=True)
+    pos = models.CharField(max_length=20, blank=True)
