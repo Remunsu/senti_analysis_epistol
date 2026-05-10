06d727f819e6d1eba0b30a7c8554da21467792c7
Mark <mark.yarullin@gmail.com>
2026-05-10 14:35:34 +0400

    added filters

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corpora',
+    "django_filters",
 ]
 
 MIDDLEWARE = [
--- backend/corpora/api.py
+++ backend/corpora/api.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Volume, Work
 from .serializers import VolumeSerializer, WorkListSerializer, WorkDetailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class VolumeViewSet(viewsets.ReadOnlyModelViewSet):
@@ -10,6 +11,38 @@ class VolumeViewSet(viewsets.ReadOnlyModelViewSet):
 
 class WorkViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Work.objects.select_related("volume").all().order_by("id")
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_fields = [
+        "volume",
+        "author",
+        "genre",
+        "language",
+        "place",
+        "date_from",
+        "date_to",
+    ]
+
+    search_fields = [
+        "title",
+        "title_short",
+        "title_desc",
+        "plain_text",
+    ]
+
+    ordering_fields = [
+        "id",
+        "title",
+        "author",
+        "genre",
+        "date_from",
+        "page_number",
+    ]
 
     def get_serializer_class(self):
         if self.action == "list":
